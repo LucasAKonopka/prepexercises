1f041c6f60bbd6f9b750081f80d8a8797641b91f
Lucas Konopka <lucasakonopka@gmail.com>
2015-06-16 21:21:20 +0000

    this is a new commit

---
+++ name.rb
@@ -0,0 +1,15 @@
+#name.rb
+
+puts 'Please enter your first name:'
+first_name = gets.chomp
+puts 'Please enter your last name:'
+last_name = gets.chomp
+
+puts first_name + ' ' + last_name
+
+puts "Hello " + first_name + " it's nice to meet you"
+
+10.times do
+  puts first_name
+end
+
