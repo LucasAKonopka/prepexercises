c41da82472a159e88f2af0028f98b946eaac2e8c
Lucas Konopka <lucasakonopka@gmail.com>
2015-06-16 17:13:33 +0000

    exercises for basics section

---
+++ exercise3.rb
@@ -0,0 +1,9 @@
+#use has to store list of movie title with their years, use puts to print out year
+
+movies = {:jurassicpark => '1990', :ghostbusters => '1982', :thehangover => '2007'}
+
+puts movies[:jurassicpark]
+puts movies[:ghostbusters]
+puts movies[:thehangover]
+
+
---
+++ exercise6.rb
@@ -0,0 +1,5 @@
+#Exercise 6
+
+puts 5.42**2
+puts 200.1**2
+puts 66.6**2
